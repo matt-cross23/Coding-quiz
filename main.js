d7b842044ea11590c77bdccf4106717d3cf224dc
Matthew Cross <matthewcross1017@gmail.com>
2021-09-26 23:20:54 -0400

    Start JS

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("I'm connected");
+//Use this to select the button to start quiz//
+var startB = document.querySelector("#start");
+
+// imgTag.setAttribute("src", "");
+// startButtn.setAttribute("", "Start Test");
+
+//Used to start quiz//
+startB.addEventListener("click", function() {
+  console.log("Maybe this will start");
+});
+// document.body.appendChild(h2);
+var timer = document.getElementById("timer");
